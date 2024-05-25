df5c8b4d2a342dc6e365fc7b870bc654003e8989
AnhNN11 <Nguyennhatanh.dev@gmail.com>
2024-05-26 00:02:08 +0700

    source base

---
+++ lib/consts/api_consts.dart
@@ -0,0 +1,7 @@
+const String BASEURL = "newsapi.org";
+const String API_KEY =
+    "ac9b3555a2184c5d8a4672e4d2f2bac0"; //84a442d2ffca4e85832a4b68696ee890
+
+// Be careful use your link
+const String BASEURL_FIREBASE =
+    "newsteaching-7edbe-default-rtdb.firebaseio.com";
---
+++ lib/consts/vars.dart
@@ -0,0 +1,22 @@
+enum NewsType {
+  topTrending,
+  allNews,
+}
+
+enum SortByEnum {
+  relevancy, // articles more closely related to q come first.
+  popularity, // articles from popular sources and publishers come first.
+  publishedAt, // newest articles come first.
+}
+
+const List<String> searchKeywords = [
+  "Football",
+  "Flutter",
+  "Python",
+  "Weather",
+  "Crypto",
+  "Bitcoin",
+  "Youtube",
+  "Netflix",
+  "Meta"
+];
